50de1f72cc385fc7c355c86b37069b08a60d8b50
Saury <fishsaury@gmail.com>
2017-04-25 23:27:44 +0800

    update the use of division in python 2.7

--- chapter01/Demo3.py
+++ chapter01/Demo3.py
@@ -8,4 +8,12 @@
 
 #0.5
 from __future__ import division
-print 1 / 2
+print 1 / 2  #---> 0.5
+
+print 1 // 2 #---> 0
+
+print 1.0 // 2 #---> 0.0
+
+print 1 // 2.0 #---> 0.0
+
+print 1.0 // 2.0 #---> 0.0
