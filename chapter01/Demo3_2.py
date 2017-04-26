f7258684413523fb376be77781d640c0a991cfa0
Saury <fishsaury@gmail.com>
2017-04-26 21:16:21 +0800

    含有负数的除法运算问题

---
+++ chapter01/Demo3_2.py
@@ -0,0 +1,18 @@
+#coding:UTF-8
+#python中的除法运算
+print 1 / 2 #---> 0
+
+print -1 / 2 #---> -1
+
+print 1 / -2 #---> -1
+
+print -1 / -2 #---> 0
+
+print 4 / 3 #---> 1
+
+print -4 / 3 #---> -2
+
+print 4 / -3 #---> -2
+
+print -4 / -3 #---> 1
+
